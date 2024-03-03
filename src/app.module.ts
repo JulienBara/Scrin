c7fbc9316ac0528c0c27f08ba5293646cea8da7d
JulienBara <bara.julien@yahoo.fr>
2024-03-03 11:49:07 +0100

    fix: fix retry

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,6 +14,7 @@ import { Device } from './devices/devices.entity';
       url: process.env.MONGODB_CONNECTION_STRING,
       database: process.env.MONGODB_DATABASE,
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      retryWrites: false,
       ssl: true,
       useUnifiedTopology: true,
       useNewUrlParser: true,
