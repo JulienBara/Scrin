733f6324d275968e3348a2ad3f2e248463d53955
JulienBara <bara.julien@yahoo.fr>
2024-03-03 10:56:16 +0100

    fix: fix test

--- src/devices/devices.controller.spec.ts
+++ src/devices/devices.controller.spec.ts
@@ -1,5 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { DevicesController } from './devices.controller';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Device } from './devices.entity';
+import { Repository } from 'typeorm';
 
 describe('DevicesController', () => {
   let controller: DevicesController;
@@ -7,6 +10,9 @@ describe('DevicesController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [DevicesController],
+      providers: [
+        { provide: getRepositoryToken(Device), useClass: Repository },
+      ],
     }).compile();
 
     controller = module.get<DevicesController>(DevicesController);
