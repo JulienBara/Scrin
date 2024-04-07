4da3943ed677e0cd88164e276c0e7ef599512571
JulienBara <bara.julien@yahoo.fr>
2024-04-07 21:37:21 +0200

    fix tests

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,12 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { JwtService } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
+import { GaufresService } from '../gaufres/gaufres.service';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: GaufresService,
+          useValue: jest.mocked({}),
+        },
+        {
+          provide: JwtService,
+          useValue: jest.mocked({}),
+        },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
