0addd901cee8f1d8f1e15664df0112dcb49bb8ef
JulienBara <bara.julien@yahoo.fr>
2024-04-07 20:57:34 +0200

    setup jwt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,20 +1,24 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { GaufresService } from '../gaufres/gaufres.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private gaufresService: GaufresService) {}
+  constructor(
+    private gaufresService: GaufresService,
+    private jwtService: JwtService,
+  ) {}
 
   async signIn(username: string, pass: string): Promise<any> {
-    const user = await this.gaufresService.findOne(username);
-    // if (user?.password !== pass) {
+    const user: any = await this.gaufresService.findOne(username);
+    // if (check password here) {
     if (true) {
       throw new UnauthorizedException();
     }
-    // const { password, ...result } = user;
-    // TODO: Generate a JWT and return it here
-    // instead of the user object
-    // return result;
-    return;
+
+    const payload = { sub: user.userId, username: user.username };
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+    };
   }
 }
